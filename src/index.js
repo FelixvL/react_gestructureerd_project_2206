1501568cb26944aa00c45b11674e67013c74b754
FelixvL <digitaleservice@gmail.com>
2022-06-29 22:36:58 +0200

    interactieve functie die dom update met interval

--- src/index.js
+++ src/index.js
@@ -5,9 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const naam = 'Felix';
-const element = <h1>Hello, {naam}</h1>;
-root.render(element);
+
+const namen = ["Fred","Joost","Karin","Maria"];
+let teller = 0;
+
+function naamWisselen() {
+
+  teller++;
+  if(teller >= namen.length){
+    teller = 0;
+  }
+
+  const element = (
+    <div>
+      <h1>Hey, Kijk!</h1>
+      <h2>It is: {namen[teller]}!</h2>
+    </div>
+  );
+
+  root.render(element);
+
+}
+
+setInterval(naamWisselen, 1000);
 
 // root.render(
 //   <React.StrictMode>
