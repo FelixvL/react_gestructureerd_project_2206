1f506cb8753514821b24bc602b70367e2dc06ac1
FelixvL <digitaleservice@gmail.com>
2022-06-29 22:24:50 +0200

    object meegegeven aan functie

--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function verwerkInfo(mijnobject) {
+  return `${mijnobject.hoogte} bij ${mijnobject.breedte} ${mijnobject.eenheid}`;
+}
+
+const verzinmaar = {
+  hoogte: 10,
+  breedte: 15,
+  eenheid: 'meter'
+};
+
+
+
 root.render(
-  <div >Hier komt iets!</div>
+  <h1>
+  Info: {verwerkInfo(verzinmaar)}!
+</h1>
 );
 // root.render(
 //   <React.StrictMode>
